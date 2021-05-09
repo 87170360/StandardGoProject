352a588c2aa786d8b8ef32ab80767cc206d81754
87170360 <87170360@qq.com>
2021-05-09 23:59:33 +0800

    .

--- cmd/demo1/main.go
+++ cmd/demo1/main.go
@@ -1,38 +1,56 @@
 package main
 
-import "fmt"
+/*
+import (
+	"fmt"
+	"os"
+	"time"
+	"github.com/google/wire"
+)
 
 func main() {
-	//e := InitializeEvent()
-	//e.Start()
+	e, err := InitializeEvent("hello")
+	if err != nil {
+		fmt.Printf("failed to create event: %s\n", err)
+		os.Exit(2)
+	}
+	e.Start()
 }
 
-func InitializeEvent() Event {
-	//wire.Build(NewEvent, NewGreeter, NewMessage)
-	return Event{}
+func InitializeEvent(phrase string) (Event, error) {
+	wire.Build(NewEvent, NewGreeter, NewMessage)
+	return Event{},nil
 }
 
 type Message string
 
-func NewMessage() Message {
-	return Message("Hi there!")
+func NewMessage(phrase string) Message {
+	return Message(phrase)
 }
 
 
 func NewGreeter(m Message) Greeter {
-	return Greeter{Message: m}
+	var grumpy bool
+	if time.Now().Unix()%2 == 0 {
+		grumpy = true
+	}
+	return Greeter{Message: m, Grumpy: grumpy}
 }
 
 type Greeter struct {
 	Message Message // <- adding a Message field
+	Grumpy bool
 }
 
 func (g Greeter) Greet() Message {
+	if g.Grumpy {
+		return Message("Go away!")
+	}
 	return g.Message
 }
 
-func NewEvent(g Greeter) Event {
-	return Event{Greeter: g}
+func NewEvent(g Greeter) (Event, error) {
+	return Event{Greeter: g}, nil
 }
 
 type Event struct {
@@ -43,3 +61,5 @@ func (e Event) Start() {
 	msg := e.Greeter.Greet()
 	fmt.Println(msg)
 }
+
+ */
