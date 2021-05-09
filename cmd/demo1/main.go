62b3fc7a37c4a1d2908600f912d509e13bf522e7
87170360 <87170360@qq.com>
2021-05-09 23:08:50 +0800

    .

--- cmd/demo1/main.go
+++ cmd/demo1/main.go
@@ -3,5 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	//e := InitializeEvent()
+	//e.Start()
+}
+
+func InitializeEvent() Event {
+	//wire.Build(NewEvent, NewGreeter, NewMessage)
+	return Event{}
+}
+
+type Message string
+
+func NewMessage() Message {
+	return Message("Hi there!")
+}
+
+
+func NewGreeter(m Message) Greeter {
+	return Greeter{Message: m}
+}
+
+type Greeter struct {
+	Message Message // <- adding a Message field
+}
+
+func (g Greeter) Greet() Message {
+	return g.Message
+}
+
+func NewEvent(g Greeter) Event {
+	return Event{Greeter: g}
+}
+
+type Event struct {
+	Greeter Greeter // <- adding a Greeter field
+}
+
+func (e Event) Start() {
+	msg := e.Greeter.Greet()
+	fmt.Println(msg)
 }
